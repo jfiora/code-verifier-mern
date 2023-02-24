e03d6e13ef213f5fedbed5d582ce7ff71637a82b
jfiora <sillonpeludo@gmail.com>
2023-02-24 15:31:09 -0300

    feat: setup swagger and tsoa

--- server/src/server/index.ts
+++ server/src/server/index.ts
@@ -1,22 +1,19 @@
-import express, { Express, Request, Response } from "express";
-import cors from "cors";
-import helmet from "helmet";
-import router from "../routes";
+import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import router from '../routes';
 
-const server:Express = express();
+const server: Express = express();
 
-server.use(
-    '/api',
-    router
-);
+server.use('/api', router);
 
+server.use(express.static('public'));
 
 server.use(helmet());
 server.use(cors());
 
-server.use(express.urlencoded({ extended: true, limit: "50mb" }));
-server.use(express.json({limit: "50mb"}));
-
+server.use(express.urlencoded({ extended: true, limit: '50mb' }));
+server.use(express.json({ limit: '50mb' }));
 
 server.get('/', (req: Request, res: Response) => {
     res.redirect('/api');
